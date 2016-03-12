113cc9980f7aa3d8cf408c701b9e9bf019160f7d
Novik166 <novik166@yandex.ru>
2016-03-12 18:29:06 +0300

    Sending message for the rival was changed. Now it uses mongo.db.

--- test.js
+++ test.js
@@ -18,13 +18,34 @@ bot.getMe().then(function(me)
 
 });
  
+
+
+// Работа с mongo.db
+    var mongoose = require('mongoose');
+
+    var userShema = mongoose.Schema({
+        first_name: String,
+        last_name: String,
+        id: Number,
+        chat_id: Number
+    })
+
+    var User = mongoose.model('User', userShema);
+    mongoose.connect('mongodb://localhost/test');
+    //mongoose.connect('mongodb://1:1@ds031842.mongolab.com:31842/xoxo');
+    mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+
+
+
+// Конец работы с mongo.db
+
 bot.on('text', function(msg)
 {
     var messageChatId = msg.chat.id;
     var messageText = msg.text;
     var messageDate = msg.date;
     var messageUsr = msg.from.first_name;
-    var messageUsrid = msg.from.id;
+    var messageUsrId = msg.from.id;
 
     var number;
     
@@ -33,13 +54,18 @@ bot.on('text', function(msg)
     }
 
     if (messageText === '/start') {
-    sendMessageByBot(messageChatId, 'Привет, ' + messageUsr + '! Меня зовут Bet bot, и я помогаю людям решать их денежные споры. Так же мы можем поиграть в игру ping-pong. Напиши мне ping.');
-        var MyUserStruct = new Object();
-        MyUserStruct.id = msg.from.id;
-        MyUserStruct.chat_id = msg.chat.id;
-        MyUserStruct.first_name = msg.from.first_name;
-        MyUserStruct.second_name =  msg.from.second_name;
-        GlobalStackUsers.push(MyUserStruct);
+
+        User.find({id: messageUsrId}, function(err, users) {
+            if (err) return console.error(err);
+            if (users.length == 0) {
+                var new_user = new User({first_name: msg.from.first_name, last_name: msg.from.last_name, id: msg.from.id, chat_id: msg.chat.id});
+                new_user.save(function(err, new_account) {
+                    if (err) return console.error(err);
+                });
+            }
+        });
+
+        sendMessageByBot(messageChatId, 'Привет, ' + messageUsr + '! Меня зовут Bet bot, и я помогаю людям решать их денежные споры. Так же мы можем поиграть в игру ping-pong. Напиши мне ping.');
     }
 
     if ((messageText == "Хочу поспорить") || (messageText == "xочу поспорить.") || (messageText == "хочу поспорить") || (messageText == "Хочу поспорить.")) {
@@ -51,27 +77,23 @@ bot.on('text', function(msg)
 
 bot.on('contact', function(msg)
 {
-    var messageUsrid = msg.from.id;
     var messageUsrFirstName = msg.from.first_name;
     var messageUsrLastName = msg.from.last_name;
     var messageUsrId = msg.from.id;
     var messageChatId = msg.chat.id;
     var rivalId = msg.contact.user_id;
-    sendMessageByBot(messageChatId, "It's his id: " + msg.contact.user_id);
+//    sendMessageByBot(messageChatId, "It's his id: " + msg.contact.user_id);
 
 //поиск по базе данных
-    var stackSize = GlobalStackUsers.length;
-    var fl = 0;
-    for (var i = 0; i < stackSize; ++i) {
-        if (GlobalStackUsers[i].id === rivalId) {
-            sendMessageByBot(GlobalStackUsers[i].chat_id, messageUsrFirstName + " " + messageUsrLastName + " хочет с вами поспорить.");
-            fl = 1;
-        }
-    }
 
-    if (fl = 0){
-        sendMessageByBot(messageChatId, "I don't know this guy.");
-    }
+    User.find({id: msg.contact.user_id}, function(err, users) {
+        console.log(users);
+        if (users.length == 0) { 
+            sendMessageByBot(messageChatId, "Я не знаю такого пользователя. Пусть он напишет мне.");
+        } else {
+            sendMessageByBot(users[0].chat_id, messageUsrFirstName + " " + messageUsrLastName + " хочет с вами поспорить.");
+        }
+    })
 
     console.log(msg);
 }); 
