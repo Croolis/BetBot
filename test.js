3bb1b171c5709a0391b2ef7277444f8ed7114e73
Novik166 <novik166@yandex.ru>
2016-03-12 12:43:23 +0300

    ping-pong

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var TelegramBot = require('node-telegram-bot-api');
 
-var token = 'YOUR_TELEGRAM_BOT_TOKEN';
+var token = '183411327:AAFbB_hqhew0k_U2wsHmdrIbCV8ben9DFuE';
+/*
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
 
@@ -14,7 +15,39 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
 // Any kind of message
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
+  var messageText = msg.text;
   // photo can be: a file path, a stream or a Telegram file_id
   var photo = 'cats.png';
-  bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
+  bot.sendMessage(chatId, Message, {caption: 'Lovely kittens'});
 });
+*/
+var botOptions = {
+    polling: true
+};
+var bot = new TelegramBot(token, botOptions);
+ 
+bot.getMe().then(function(me)
+{
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
+});
+ 
+bot.on('text', function(msg)
+{
+    var messageChatId = msg.chat.id;
+    var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
+ 
+    if (messageText === 'ping') {
+        sendMessageByBot(messageChatId, 'pong');
+    }
+    
+    console.log(msg);
+});
+ 
+function sendMessageByBot(aChatId, aMessage)
+{
+    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+}
